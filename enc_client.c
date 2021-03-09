f80eacbe52453c9f5da795b887f00c8aa51f75c9
Connor Bentzley <bentzlec@os1.engr.oregonstate.edu>
2021-03-08 16:20:04 -0800

    Finished enc_client.c. Currently have warning initialization makes integer from pointer without a cast from fopen on line 90

--- enc_client.c
+++ enc_client.c
@@ -8,6 +8,28 @@
 #include <netdb.h>
 #include <ctype.h>
 
+int getFileLength(char * fileName) {
+   int curr, numChars = 0;
+   
+   FILE* fd = fopen(fileName, "r"); //Open given filename
+
+   curr = fgetc(fd); //Get the very first character in the file
+
+   while (curr != EOF || curr != '\n') { //Read until end of file or newline
+      if(!isupper(curr) && curr != ' ') { //Check for bad characters
+	    fprintf(stderr, "File contains bad characters!");
+	    fflush(stdout);
+	    exit(1);
+      }
+      curr = fgetc(fd); //Get the next character
+
+      numChars++; //Iterate counter
+   } 
+   fclose(fd); //Close file
+
+   return numChars;
+}
+
 int main (int argc, char * argv[]) {
    int sockFD, portNum, charWrittenIdx, charReadIdx, bytesRead = 0;
    struct sockaddr_in sockAdd;
@@ -21,19 +43,74 @@ int main (int argc, char * argv[]) {
    sockAdd.sin_family = AF_INET; //Make it accept connections
    sockAdd.sin_port = htons(portNum); //Put port in struct
    serverHost = gethostbyname("localhost"); //Get the address
-   //Error check here?
    memcpy((char*)&sockAdd.sin_addr.s_addr, (char*)serverHost->h_addr, serverHost->h_length); //Copy address into struct
 
+
    sockFD = socket(AF_INET, SOCK_STREAM, 0); //Make the socket
-   //Error check socket?
+   //Error check socket
+   if(sockFD < 0) {
+      printf("Socket error!\n");
+      fflush(stdout);
+      exit(1);
+   }
    
+   //Make connection to server
    if(connect(sockFD, (struct sockaddr*)&sockAdd, sizeof(sockAdd)) < 0) {
-      error("Client error!\n");
+      printf("Client side error!\n");
+      fflush(stdout);
+      exit(1);
    }
 
+   long fileLen = getFileLength(argv[1]); //Get file lengths
+   long keylength = getFileLength(argv[2]);
 
+   if(fileLen > keylength) { //Error handle when key is too short
+      printf("Error! Key too short!\n");
+      fflush(stdout);
+      exit(1);
+   }
 
+   char msg[] = "Handshake"; //Create handshake message
+   
+   while(charWrittenIdx == 0) {
+      charWrittenIdx = recv(sockFD, buff, sizeof(buff) - 1, 0);
+   }
 
+   if(strcmp(buff, "Bad Port") == 0) { //Error handle bad port
+      printf("Bad port number!\n");
+      fflush(stdout);
+      exit(1);
+   }
+
+   memset(buff, '\0', sizeof(buff)); //Clear buffer
+   sprintf(buff, "%d", fileLen); //put the file length in the buffer
+   charWrittenIdx = send(sockFD, buff, sizeof(buff), 0); //Send the file len
+   memset(buff, '\0', sizeof(buff)); //Clear buffer again
+
+   int fd = fopen(argv[1], "r"); //Open the file
+   if(fd < 0) { //Error handling for bad file open
+      printf("Error opening file!\n");
+      fflush(stdout);
+      exit(1);
+   }
+
+   int i = 0;
+   while(i <= fileLen) { //Read to end of file, track for both file read and file send
+      bytesRead = read(fd, buff, sizeof(buff) - 1);
+      i += send(sockFD, buff, strlen(buff), 0);
+   }
+
+   memset(buff, '\0', sizeof(buff)); //Clear buffer
+
+   i = 0;
+   while(i <= fileLen) {
+      i += recv(sockFD, buff, sizeof(buff) - 1, 0); //Receive and store in buffer and print to screen
+      strcat(cipherBuff, buff);
+   }
+   strcat(cipherBuff, "\n");
+   printf("%s", cipherBuff);
+
+   close(sockFD);
 
    return 0;
 }
